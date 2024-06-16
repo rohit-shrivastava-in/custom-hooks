3153d94fdaf5675c683522d83c4b912788791426
Rohit Shrivastava <rohit.shri2389@gmail.com>
2024-06-16 18:07:32 +0530

    useAsync useFetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import PreviousComponent from "./1-usePrevious/PreviousComponent"
-import TranslationComponent from "./3 useTranslation/TranslationComponent"
+// import TranslationComponent from "./3-useTranslation/TranslationComponent"
+// import AsyncComponent from "./4-useAsync/AsyncComponent"
+import FetchComponent from "./5-useFetch/FetchComponent"
 
 function App() {
   return (
     // <PreviousComponent />
-    <TranslationComponent />
+    // <TranslationComponent />
+    // <AsyncComponent />
+    <FetchComponent />
   );
 }
 
